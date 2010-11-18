9d64809f9ecc9ed441152d00400b870df76f20e9
chris schepman <cschep@gmail.com>
2010-11-18 09:50:49 -0800

    high res tab bar icons

---
+++ Classes/TwitterViewController.h
@@ -0,0 +1,16 @@
+//
+//  TwitterViewController.h
+//  Baby Ketten
+//
+//  Created by Christopher Schepman on 10/16/10.
+//  Copyright 2010 schepsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TwitterViewController : UIViewController <UIWebViewDelegate> {
+	IBOutlet UIWebView *webView;
+}
+
+@end
---
+++ Classes/TwitterViewController.m
@@ -0,0 +1,62 @@
+//
+//  TwitterViewController.m
+//  Baby Ketten
+//
+//  Created by Christopher Schepman on 10/16/10.
+//  Copyright 2010 schepsoft. All rights reserved.
+//
+
+#import "TwitterViewController.h"
+
+
+@implementation TwitterViewController
+
+/*
+ // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+	NSURL *url = [NSURL URLWithString:@"http://bkk.schepman.org/twitter.html"];
+	NSURLRequest *request = [NSURLRequest requestWithURL:url];
+	[webView loadRequest:request];
+	
+    [super viewDidLoad];
+}
+
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
